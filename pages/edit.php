40ffde6bac217d6e66eeac64420b9bb1f6546c26
Enrique Rodriguez <211056022+EnriqueR-tech@users.noreply.github.com>
2026-03-19 14:37:05 -0500

    Update on CRUD
    Minor tweak to fix HH:MM:SS format from HTML/PHP to MySQL that matches the same format (except the AM/PM part)
    
    Dont know why the index.php input throws the wrong value but its a pass....

--- pages/edit.php
+++ pages/edit.php
@@ -5,6 +5,8 @@ include "../connection.php";
 //Grabbing the table ID from the URL and matching it with the database to get the data of the specific row
 if (isset($_GET["id"])){
     $id = $_GET["id"];
+
+    //select the movie details table and get the row with the matching ID
     $sql = "SELECT * FROM `movie details` WHERE movie_id=$id";
     $result = $connection->query($sql);
     if (!$result){
@@ -53,13 +55,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         <form action="edit.php" method="post">
             <input type="hidden" name="id" value="<?php echo $id; ?>">
             <label>Title</label>
-            <input type="text" name="title" value=" <?php echo $row['Title']; ?>">
+            <input type="text" name="title" value="<?php echo $row['Title']; ?>">
         
             <label>Runtime</label>
-            <input type="text" name="runtime" value=" <?php echo $row['Runtime']; ?>">
+            <input type="time" name="runtime" step="1" value="<?php echo $row['Runtime']; ?>">
 
             <label>Rating</label>
-            <input type="text" name="rating" value=" <?php echo $row['Rating']; ?>">
+            <input type="text" name="rating" value="<?php echo $row['Rating']; ?>">
 
             <label>Description</label>
             <input type="text" name="description" value=" <?php echo $row['Description']; ?>">
