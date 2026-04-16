4066798f8be5301372eb8b39c38e9833b71b9cac
Enrique Rodriguez <211056022+EnriqueR-tech@users.noreply.github.com>
2026-04-15 20:05:33 -0500

    Sample Data Update
    Added new table data on `movies`
    
    renamed 3 tables of: `movies` `tickets` `screenings`
    
    Co-Authored-By: Jesus <97010170+JesusDtx@users.noreply.github.com>

--- database/movie_site_db.sql
+++ database/movie_site_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2026 at 04:48 AM
+-- Generation Time: Apr 16, 2026 at 03:04 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,10 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `movies` (
   `movie_id` int(11) NOT NULL,
-  `Title` varchar(200) NOT NULL,
-  `Runtime` time NOT NULL,
-  `Rating` decimal(3,1) NOT NULL,
-  `Description` text NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `runtime` time NOT NULL,
+  `rating` decimal(3,1) NOT NULL,
+  `description` text NOT NULL,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Storing movie info';
 
@@ -40,7 +40,7 @@ CREATE TABLE `movies` (
 -- Dumping data for table `movies`
 --
 
-INSERT INTO `movies` (`movie_id`, `Title`, `Runtime`, `Rating`, `Description`, `image`) VALUES
+INSERT INTO `movies` (`movie_id`, `title`, `runtime`, `rating`, `description`, `image`) VALUES
 (1, 'A Minecraft Movie', '01:41:00', 5.6, 'Four misfits are suddenly pulled through a mysterious portal into a bizarre cubic wonderland that thrives on imagination. To get back home they\'ll have to master this world while embarking on a quest with an unexpected expert crafter.', 'AMinecraftMovie.jpg'),
 (2, 'Summer Wars', '01:54:00', 7.4, 'A student tries to fix a problem he accidentally caused in OZ, a digital world, while pretending to be the fiancé of his friend at her grandmother\'s 90th birthday.', 'SummerWars.jpg'),
 (3, 'Point Break', '02:51:02', 7.2, 'An F.B.I. Agent goes undercover to catch a gang of surfers who may be bank robbers.', 'PointBreak.jpg'),
@@ -50,31 +50,34 @@ INSERT INTO `movies` (`movie_id`, `Title`, `Runtime`, `Rating`, `Description`, `
 (9, 'John Wick', '01:40:00', 10.0, '     Hello World Testing 4', ''),
 (10, 'Unbreakable(200)', '01:47:00', 10.0, '   A man learns something extraordinary about himself after a devastating accident.', ''),
 (12, 'Sasuage Party', '02:00:00', 2.0, ' A terrible rated R movie that tricked parents thinking this is a cute PG-13 movie with cutesy food characters, but it contain ADULT THEME AND ADULT HUMOR (college humor)', 'SasuageParty.jpg'),
-(14, 'Project Hail Mary', '02:03:50', 8.0, '  Science teacher Ryland Grace (Ryan Gosling) wakes up on a spaceship light years from home with no recollection of who he is or how he got there. As his memory returns, he begins to uncover his mission: solve the riddle of the mysterious substance causing the sun to die out. He must call on his scientific knowledge and unorthodox ideas to save everything on Earth from extinction… but an unexpected friendship means he may not have to do it alone.', '');
+(14, 'Project Hail Mary', '02:03:50', 8.0, '  Science teacher Ryland Grace (Ryan Gosling) wakes up on a spaceship light years from home with no recollection of who he is or how he got there. As his memory returns, he begins to uncover his mission: solve the riddle of the mysterious substance causing the sun to die out. He must call on his scientific knowledge and unorthodox ideas to save everything on Earth from extinction… but an unexpected friendship means he may not have to do it alone.', ''),
+(17, 'Ghost In The Shell', '01:22:40', 8.0, ' In the year 2029, the world has become interconnected by a vast electronic network that permeates every aspect of life. That same network also becomes a battelfield for Tokyo\'s Section Nine security force, which has been charged with apprehending the master hacker known only as the Puppet Master. Spearheading the investigation is Major Motoko Kusanagi, who - like many in her department - is a cyborg officer, far more powerful than her human appearance would suggest. And yet as the Puppet Master, who is even capable of hacking human minds, leaves a trail of victims robbed of their memories. Motoko begins to ponder the very nature of her existence: is she purely an artificial construct, or is there more? What, exactly, is the ', 'GhostInTheShell.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `screening`
+-- Table structure for table `screenings`
 --
 
-CREATE TABLE `screening` (
+CREATE TABLE `screenings` (
   `id` int(11) NOT NULL,
   `movie_id` int(11) NOT NULL,
+  `theater_name` varchar(255) NOT NULL,
   `start_time` datetime NOT NULL,
-  `end_time` datetime NOT NULL
+  `end_time` datetime NOT NULL,
+  `capacity` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `screening`
+-- Dumping data for table `screenings`
 --
 
-INSERT INTO `screening` (`id`, `movie_id`, `start_time`, `end_time`) VALUES
-(1, 1, '2026-04-07 12:00:00', '2026-04-07 15:00:00'),
-(2, 2, '2026-04-09 15:00:00', '2026-04-09 17:00:00'),
-(3, 14, '2026-04-09 12:00:00', '2026-04-09 15:00:00'),
-(4, 2, '2026-04-07 16:00:00', '2026-04-07 17:00:00'),
-(5, 1, '2026-04-06 14:00:00', '2026-04-06 16:00:00');
+INSERT INTO `screenings` (`id`, `movie_id`, `theater_name`, `start_time`, `end_time`, `capacity`) VALUES
+(1, 1, 'AMC DINE-IN Mesquite 30', '2026-04-07 12:00:00', '2026-04-07 15:00:00', 100),
+(2, 2, 'AMC Center 12', '2026-04-09 15:00:00', '2026-04-09 17:00:00', 100),
+(3, 14, 'AMC Heights 8', '2026-04-09 12:00:00', '2026-04-09 15:00:00', 100),
+(4, 2, 'AMC DINE-IN Mesquite 30', '2026-04-07 16:00:00', '2026-04-07 17:00:00', 98),
+(5, 1, 'AMC Plaza 15', '2026-04-06 14:00:00', '2026-04-06 16:00:00', 100);
 
 -- --------------------------------------------------------
 
@@ -87,6 +90,7 @@ CREATE TABLE `tickets` (
   `movie_id` int(11) NOT NULL,
   `customer_name` varchar(100) NOT NULL,
   `tickets` int(11) NOT NULL,
+  `theater_loc` varchar(255) NOT NULL,
   `show_date` date NOT NULL,
   `show_time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='To store movie tickets and connect table to movie details';
@@ -95,8 +99,9 @@ CREATE TABLE `tickets` (
 -- Dumping data for table `tickets`
 --
 
-INSERT INTO `tickets` (`id`, `movie_id`, `customer_name`, `tickets`, `show_date`, `show_time`) VALUES
-(1, 2, 'Enrique R', 1, '2026-03-27', '15:00:00');
+INSERT INTO `tickets` (`id`, `movie_id`, `customer_name`, `tickets`, `theater_loc`, `show_date`, `show_time`) VALUES
+(1, 2, 'Enrique R', 1, '', '2026-03-27', '15:00:00'),
+(2, 2, 'NEMO', 1, 'AMC DINE-IN Mesquite 30', '2026-04-07', '16:00:00');
 
 --
 -- Indexes for dumped tables
@@ -109,9 +114,9 @@ ALTER TABLE `movies`
   ADD PRIMARY KEY (`movie_id`);
 
 --
--- Indexes for table `screening`
+-- Indexes for table `screenings`
 --
-ALTER TABLE `screening`
+ALTER TABLE `screenings`
   ADD PRIMARY KEY (`id`),
   ADD KEY `movie_id` (`movie_id`);
 
@@ -130,29 +135,29 @@ ALTER TABLE `tickets`
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `screening`
+-- AUTO_INCREMENT for table `screenings`
 --
-ALTER TABLE `screening`
+ALTER TABLE `screenings`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `screening`
+-- Constraints for table `screenings`
 --
-ALTER TABLE `screening`
-  ADD CONSTRAINT `screening_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`);
+ALTER TABLE `screenings`
+  ADD CONSTRAINT `screenings_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`);
 
 --
 -- Constraints for table `tickets`
